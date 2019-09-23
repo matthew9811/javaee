980206116d921f025679fecbd5c57f6047b7e1b1
matthew <matthewvictorialeung@outlook.com>
2019-09-23 15:29:40 +0800

    修改登录页面位置

--- carblog/src/main/java/com/shengxi/carblog/controller/main/RestErrorController.java
+++ carblog/src/main/java/com/shengxi/carblog/controller/main/RestErrorController.java
@@ -1,8 +1,6 @@
 package com.shengxi.carblog.controller.main;
 
 import org.apache.catalina.connector.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
