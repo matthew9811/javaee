4ee8b5c4fa67922dcd0b1f3e65c8054004db9fd7
yan <1277218464@qq.com>
2019-10-23 16:15:50 +0800

    配置前端静态资源配置

--- carblog/src/main/java/com/shengxi/carblog/ServletInitializer.java
+++ carblog/src/main/java/com/shengxi/carblog/ServletInitializer.java
@@ -5,6 +5,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author yan
@@ -12,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
  * 增加启动提示
  */
 @Configuration
-public class ServletInitializer extends SpringBootServletInitializer implements ApplicationRunner {
+public class ServletInitializer extends SpringBootServletInitializer implements WebMvcConfigurer, ApplicationRunner {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -22,5 +24,12 @@ public class ServletInitializer extends SpringBootServletInitializer implements 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("BUILD SUCCESS!");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+        .addResourceLocations("classpath:/resources/");
     }
 }
