e2f47585477ad84fee5625a1920e95b9e9791551
yan <1277218464@qq.com>
2019-10-23 15:34:08 +0800

    修改启动提示,配置前端应用路径

--- carblog/src/main/java/com/shengxi/carblog/ServletInitializer.java
+++ carblog/src/main/java/com/shengxi/carblog/ServletInitializer.java
@@ -1,13 +1,26 @@
 package com.shengxi.carblog;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+/**
+ * @author yan
+ * 快速启动
+ * 增加启动提示
+ */
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer implements ApplicationRunner {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(CarblogApplication.class);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("BUILD SUCCESS!");
+    }
 }
