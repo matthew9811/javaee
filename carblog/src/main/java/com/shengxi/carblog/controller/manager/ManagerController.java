0aeff68df1c74e4666d655cded27c438c361ac4b
yan <1277218464@qq.com>
2019-11-20 11:27:55 +0800

    增加后台管理控制器

---
+++ carblog/src/main/java/com/shengxi/carblog/controller/manager/ManagerController.java
@@ -0,0 +1,15 @@
+package com.shengxi.carblog.controller.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 后台控制器
+ * @author yan
+ * @version 1.0.1
+ * @date 2019-11-20 11:26:21
+ */
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+}
